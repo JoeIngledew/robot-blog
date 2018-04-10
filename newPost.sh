01531e5592ad1683b03685b916132454b4a2a272
Joe Ingledew <ingledewjf@gmail.com>
2018-04-10 10:32:34 +0000

    i am good at shell honest

--- newPost.sh
+++ newPost.sh
@@ -1,8 +1,21 @@
 year="$(date +'%Y')"
 month="$(date +'%m')"
 
+
 echo "path: content/posts/$year/$month/$1.md"
 echo "Opening a post named $1 for edit."
 
+file=content/posts/$year/$month/$1.md
+echo "creating $file"
+
 mkdir -p content/posts/$year/$month
-vi content/posts/$year/$month/$1.md
+touch $file
+
+echo "Title:" >> $file
+echo "Date: $(date +'%Y-%m-%d %H:%M')" >> $file
+echo "Slug: $1" >> $file
+echo "Author: Ro Avery" >> $file
+echo "Category:" >> $file
+echo "Tags:" >> $file
+
+vi $file
